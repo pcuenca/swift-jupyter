81574b5743029bc3e304ec0b107af509574b7edd
Pedro Cuenca <pedro@latenitesoft.com>
2019-04-09 08:23:50 +0000

    Share package installation directory across notebooks in the same Python environment.
    This allows build intermediate files to be shared. In the typical case
    when a user is restarting the same notebook multiple times, the install
    time of the first cell that contains all package dependencies will be
    reduced.
    
    To create some isolation but still provide some utility, a different
    package installation directory is used per Python environment. The
    directory is determined by guessing the type of environment the
    notebook is running in. The following scenarios have been considered so
    far:
    
    - Python 3 running inside a virtualenv. The installation directory will
    be a sibling of the directory that contains the environment.
    - Anaconda. In this case, the installation directory will be stored
    inside the Anaconda environment directory.
    - System-wide Python or unknown environment. The ~/swift-env directory
    will be used.
    
    Limitations and To-Dos:
    * Two notebooks running at the same time inside the same environment
    might still clobber each other.
    * Ideally, we should provide a way for the user to override the
    installation directory and define an arbitrary one.
    * Check how to identify Python2 + virtualenv.

--- parent_kernel.py
+++ parent_kernel.py
@@ -23,6 +23,36 @@ import subprocess
 import sys
 import tempfile
 
+# Determine the directory where user-installed packages will reside.
+# Current locations include:
+#   . <virtualenv_base>/swift-env, when using a virtualenv environment.
+#     For example, if the virtualenv resides in ~/projects/myproject/env,
+#     the location will be ~/projects/myproject/swift-env.
+#     <virtualenv_base> is the directory that in turn contains the virtual environment.
+#   . <conda_env>/swift-env, when using Anaconda.
+#     For example, if the conda environment is called 'test', the location
+#     will look like ~/anaconda3/envs/test/swift-env.
+#   . ~/swift-env, if a system-wide Python is used or no virtual environment
+#     container was detected. No attempt at separating Python versions
+#     is performed.
+def get_swift_packages_location():
+    # Guess type of environment and derive an appropriate dir accordingly
+    env_name = 'swift-env'
+
+    # Anaconda
+    if 'CONDA_PREFIX' in os.environ:
+        return os.path.join(os.environ['CONDA_PREFIX'], env_name)
+
+    # virtualenv, Python 3
+    if hasattr(sys, 'base_prefix'):
+        if sys.prefix != sys.base_prefix:
+            return os.path.join(os.path.dirname(sys.prefix), env_name)
+
+    # ToDo: verify whether Python 2 running in virtualenv exposes base_prefix
+
+    # Unknown or system-wide
+    return os.path.join(os.path.expanduser('~'), env_name)
+
 # The args to launch "swift_kernel.py" are the same as the args we received,
 # except with "parent_kernel.py" replaced with "swift_kernel.py".
 args = [
@@ -31,14 +61,15 @@ args = [
 ]
 args += sys.argv[1:]
 
-# Construct a temporary directory for package installation scratchwork. This
-# must happen in the parent process because we need to set the
-# SWIFT_IMPORT_SEARCH_PATH environment in the child to tell LLDB where module
-# files go.
-package_install_scratchwork_base = tempfile.mkdtemp()
-swift_import_search_path = os.path.join(package_install_scratchwork_base,
+# Determine a directory for package installation. This must happen in the
+# parent process because we need to set the SWIFT_IMPORT_SEARCH_PATH
+# environment in the child to tell LLDB where module files go.
+package_install_base = get_swift_packages_location()
+swift_import_search_path = os.path.join(package_install_base,
                                         'modules')
 os.makedirs(swift_import_search_path, exist_ok=True)
+
+print('Using Swift packages directory:', package_install_base )
 
 # Launch "swift_kernel.py".
 process = subprocess.Popen(
